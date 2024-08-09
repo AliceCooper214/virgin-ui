0e8185c41e44204d7716d61753c564e30c14d706
cooper <openbilibili@126.com>
2024-08-09 12:50:34 +0800

    chore: 🔨  rename file

--- src/button/Button.stories.tsx
+++ src/button/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import Button from "./";
+import Button from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
--- src/button/index.test.tsx
+++ src/button/index.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-import Button from "./index";
+import Button from "./Button";
 
 describe("Button", () => {
   test("renders Button", () => {
