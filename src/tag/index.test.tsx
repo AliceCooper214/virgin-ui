73502952804668fea5e82464ae177f5b3d550a57
cooper <openbilibili@126.com>
2024-08-10 02:42:49 +0800

    test: 🚨  add test for tag component

--- src/tag/index.test.tsx
+++ src/tag/index.test.tsx
@@ -1,36 +1,27 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import Tag from './index';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Tag from "./index";
 
-describe('Tag', () => {
-  test('renders Tag', () => {
+describe("Tag", () => {
+  test("renders Tag", () => {
     render(<Tag>click me</Tag>);
     const linkElement = screen.getByText(/click me/i);
+    // @ts-ignore
     expect(linkElement).toBeInTheDocument();
   });
-  
-/**
-  test('renders normal Tag', () => {
-    const { container } = render(<Tag>click me</Tag>);
-  
-    expect(container.querySelector('.ant-btn-normal')).toBeInTheDocument();
+
+  test("should support onClose", () => {
+    const onClose = jest.fn();
+    const { container } = render(
+      <Tag closable onClose={onClose}>
+        tag
+      </Tag>
+    );
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const linkElement = container.querySelector(".anticon");
+    fireEvent.click(linkElement!);
+
+    expect(onClose).toBeCalled();
   });
-
-  test('renders small Tag', () => {
-    const { container } = render(<Tag size="small">click me</Tag>);
-  
-    expect(container.querySelector('.ant-btn-small')).toBeInTheDocument();
-  });
-
-  test('should support click', () => {
-    const onClick = jest.fn();
-    render(<Tag type="primary" onClick={onClick}>click me</Tag>);
-
-    const linkElement = screen.getByText(/click me/i);
-    fireEvent.click(linkElement);
-
-    expect(onClick).toBeCalled();
-  });
-  **/
 });
-
