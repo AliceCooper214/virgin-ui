9c1ddcf8a5f2066492432e16bb6327c9fd9ccffc
cooper <openbilibili@126.com>
2024-08-10 21:04:43 +0800

    feat: ✨  add shell

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 查看当前目录
+pwd
+
+# 生成静态文件
+pnpm run build:components
+pnpm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy to the gh-pages'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f https://github.com/AliceCooper214/virgin-ui.git master:gh-pages
