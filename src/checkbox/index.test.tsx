dd4d6e04abaa7fdd599e615737f75a62fbd8dc7e
cooper <openbilibili@126.com>
2024-08-11 03:05:04 +0800

    test: 🚨  add unit test to checkbox

--- src/checkbox/index.test.tsx
+++ src/checkbox/index.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+
 import Checkbox from "./index";
+import CheckboxGroup from "./checkbox-group";
 
 describe("Checkbox", () => {
   test("renders Checkbox", () => {
@@ -8,5 +10,20 @@ describe("Checkbox", () => {
     const linkElement = screen.getByText(/click me/i);
     // @ts-ignore
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test("should support click", () => {
+    const onChange = jest.fn();
+    render(
+      <CheckboxGroup onChange={onChange}>
+        <Checkbox value="1">click me</Checkbox>
+        <Checkbox value="2">click 2</Checkbox>
+      </CheckboxGroup>
+    );
+
+    const linkElement = screen.getByText(/click me/i);
+    fireEvent.click(linkElement);
+
+    expect(onChange).toBeCalled();
   });
 });
