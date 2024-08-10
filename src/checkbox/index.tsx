1508cdefb0f4f63c38e231761bafd717891f822f
cooper <openbilibili@126.com>
2024-08-11 04:07:01 +0800

    refactor: 📦️  modify index on checkbox

--- src/checkbox/index.tsx
+++ src/checkbox/index.tsx
@@ -1,3 +1,11 @@
-import Checkbox from './checkbox';
+import InternalCheckbox from "./checkbox";
+import Group from "./checkbox-group";
 
+type CheckboxType = typeof InternalCheckbox;
+interface CheckboxInterface extends CheckboxType {
+  Group: typeof Group;
+}
+
+const Checkbox = InternalCheckbox as CheckboxInterface;
+Checkbox.Group = Group;
 export default Checkbox;
